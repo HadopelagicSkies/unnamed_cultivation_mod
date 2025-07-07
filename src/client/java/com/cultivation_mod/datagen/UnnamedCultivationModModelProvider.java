09b8cd0d2fbabc14d2b74d6c4e08b50bbba2dbe0
HadopelagicSkies <hadopelagicskies@gmail.com>
2025-07-07 14:34:18 -0400

    jade block variants mostly working, need to fix wall connecting, inventory model

--- src/client/java/com/cultivation_mod/datagen/UnnamedCultivationModModelProvider.java
+++ src/client/java/com/cultivation_mod/datagen/UnnamedCultivationModModelProvider.java
@@ -4,10 +4,10 @@ import com.cultivation_mod.CultivationModBlocks;
 import com.cultivation_mod.CultivationModItems;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.data.BlockStateModelGenerator;
-import net.minecraft.client.data.ItemModelGenerator;
-import net.minecraft.client.data.Models;
+import net.minecraft.client.data.*;
+import net.minecraft.util.Identifier;
 
 public class UnnamedCultivationModModelProvider extends FabricModelProvider {
     public UnnamedCultivationModModelProvider(FabricDataOutput output) {
@@ -16,19 +16,23 @@ public class UnnamedCultivationModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        // jade block stairs
-        // jade block slab
-        // jade block wall
+
+        registerStairs(blockStateModelGenerator,CultivationModBlocks.JADE_BLOCK_STAIRS,CultivationModBlocks.JADE_BLOCK);
+        registerSlab(blockStateModelGenerator,CultivationModBlocks.JADE_BLOCK_SLAB,CultivationModBlocks.JADE_BLOCK);
+        registerWall(blockStateModelGenerator,CultivationModBlocks.JADE_BLOCK_WALL,CultivationModBlocks.JADE_BLOCK);
+
         blockStateModelGenerator.registerSimpleCubeAll(CultivationModBlocks.JADE_BRICKS);
-        // jade brick stairs
-        // jade brick slab
-        // jade brick wall
+        registerStairs(blockStateModelGenerator,CultivationModBlocks.JADE_BRICK_STAIRS,CultivationModBlocks.JADE_BRICKS);
+        registerSlab(blockStateModelGenerator,CultivationModBlocks.JADE_BRICK_SLAB,CultivationModBlocks.JADE_BRICKS);
+        registerWall(blockStateModelGenerator,CultivationModBlocks.JADE_BRICK_WALL,CultivationModBlocks.JADE_BRICKS);
+
         blockStateModelGenerator.registerSimpleCubeAll(CultivationModBlocks.CRACKED_JADE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(CultivationModBlocks.MOSSY_JADE_BRICKS);
+
         blockStateModelGenerator.registerSimpleCubeAll(CultivationModBlocks.POLISHED_JADE_BLOCK);
-        // polished jade block stairs
-        // polished jade block slab
-        // polished jade block wall
+        registerStairs(blockStateModelGenerator,CultivationModBlocks.POLISHED_JADE_BLOCK_STAIRS,CultivationModBlocks.POLISHED_JADE_BLOCK);
+        registerSlab(blockStateModelGenerator,CultivationModBlocks.POLISHED_JADE_BLOCK_SLAB,CultivationModBlocks.POLISHED_JADE_BLOCK);
+        registerWall(blockStateModelGenerator,CultivationModBlocks.POLISHED_JADE_BLOCK_WALL,CultivationModBlocks.POLISHED_JADE_BLOCK);
     }
 
 
@@ -55,5 +59,24 @@ public class UnnamedCultivationModModelProvider extends FabricModelProvider {
     @Override
     public String getName() {
         return "Unnamed Cultivation Mod Model Provider";
+    }
+
+    public void registerStairs(BlockStateModelGenerator blockStateModelGenerator, Block block, Block baseBlock){
+        blockStateModelGenerator.blockStateCollector.accept(BlockStateModelGenerator.createStairsBlockState(block,
+                Models.INNER_STAIRS.upload(block, TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.STAIRS.upload(block,TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.OUTER_STAIRS.upload(block, TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector)));
+    }
+    public void registerWall(BlockStateModelGenerator blockStateModelGenerator, Block block, Block baseBlock){
+        blockStateModelGenerator.blockStateCollector.accept(BlockStateModelGenerator.createWallBlockState(block,
+                Models.TEMPLATE_WALL_POST.upload(block, TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.TEMPLATE_WALL_SIDE.upload(block,TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.TEMPLATE_WALL_SIDE_TALL.upload(block, TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector)));
+    }
+    public void registerSlab(BlockStateModelGenerator blockStateModelGenerator, Block block, Block baseBlock){
+        blockStateModelGenerator.blockStateCollector.accept(BlockStateModelGenerator.createSlabBlockState(block,
+                Models.SLAB.upload(block,TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.SLAB_TOP.upload(block,TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector),
+                Models.CUBE_ALL.upload(block,"_full",TextureMap.all(baseBlock),blockStateModelGenerator.modelCollector)));
     }
 }
